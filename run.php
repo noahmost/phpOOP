79ac1487d19c43528b5128c7f6f0371e2401b7c2
Noah Mostacero <noahmostacero@gmail.com>
2015-01-08 11:17:02 -0800

    Lecture 3 examples

---
+++ run.php
@@ -0,0 +1,34 @@
+<?php
+//    variables
+$kangaroo = true;
+$rubierudie = null;
+$huehuehue = 21;
+
+//    Example 1
+
+if (is_bool($kangaroo)=== true) {
+    echo "Yes this is uno booloono";
+}
+else {
+    echo "Not a booloon";
+}
+
+//    Example 2
+
+
+if (is_null($rubierudie)=== true) {
+    echo "Yes this is un nullo";
+}
+else {
+    echo "Not un nullo";
+}
+
+
+//    Example 3
+
+if (is_integer($huehuehue)=== true) {
+    echo "Yes this is one integer";
+}
+else {
+    echo "Not one integer";
+}
